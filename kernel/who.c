34477f5d460a25fae62dac57cbdbbb398bf5e835
Ming-Y-ANG <714536723@qq.com>
2023-05-13 10:52:10 +0800

    add sys_call: iam() & whoami()

---
+++ kernel/who.c
@@ -0,0 +1,44 @@
+#include <asm/segment.h>
+#include <errno.h>
+#include <string.h>
+
+char _myname[24];
+
+int sys_iam(const char *name)
+{
+    char str[25];
+    int i = 0;
+
+    do{
+        // get char from user input
+        str[i] = get_fs_byte(name + i);
+    } while (i <= 25 && str[i++] != '\0');
+
+    if (i > 24){
+        errno = EINVAL;
+        i = -1;
+    }else{
+        // copy from user mode to kernel mode
+        strcpy(_myname, str);
+    }
+
+    return i;
+}
+
+int sys_whoami(char *name, unsigned int size)
+{
+    int length = strlen(_myname);
+    int i = 0;
+
+    if (size < length){
+        errno = EINVAL;
+        length = -1;
+    }else{
+        for (i = 0; i < length; i++){
+            // copy from kernel mode to user mode
+            put_fs_byte(_myname[i], name + i);
+        }
+    }
+
+    return length;
+}
